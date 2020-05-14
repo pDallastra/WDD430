aa1e20d911352cd0b2c877685e8f1b5b9e6ebcfa
pDallastra <prpdallastra@gmail.com>
2020-05-14 09:36:04 -0600

    Week 04 - Done

--- week02/cms/src/app/shared/dropdown.directive.ts
+++ week02/cms/src/app/shared/dropdown.directive.ts
@@ -6,7 +6,15 @@ import { Directive, HostListener, HostBinding } from '@angular/core';
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
 
-  @HostListener('click') toggleOpen() {
+  @HostListener('mouseover') toggleOpen() {
     this.isOpen = !this.isOpen;
+  }
+
+  @HostListener('click') toggleChange() {
+    this.isOpen = !this.isOpen;
+  }
+
+  @HostListener('mouseout') toggleClose() {
+    this.isOpen = false;
   }
 }
