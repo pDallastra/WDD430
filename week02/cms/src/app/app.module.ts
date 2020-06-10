9359d311e854903d4822df412dae195cfee6f03c
pDallastra <prpdallastra@gmail.com>
2020-06-09 19:47:11 -0600

    Copy Files placed! - W08

--- week02/cms/src/app/app.module.ts
+++ week02/cms/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
@@ -42,7 +43,8 @@ import { ContactEditComponent } from './contacts/contact-edit/contact-edit.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
